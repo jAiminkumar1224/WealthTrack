bf8b2fc1f988f00bb06ae7a40733006a0ffbfc9d
Jaiminkumar Patel <cap.jemspatel1212@gmail.com>
2025-07-02 23:51:08 +0530

    Firebase Setup for Authtentication

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgvkv6Zx_5c35AjhZ0hqbEyz3Ve6Zq-UM",
+    authDomain: "wealthtrack-fed98.firebaseapp.com",
+    projectId: "wealthtrack-fed98",
+    storageBucket: "wealthtrack-fed98.firebasestorage.app",
+    messagingSenderId: "545253308918",
+    appId: "1:545253308918:web:ae9abd813f1b8f6a5befd7"
+};
+
+const app = initializeApp(firebaseConfig);
